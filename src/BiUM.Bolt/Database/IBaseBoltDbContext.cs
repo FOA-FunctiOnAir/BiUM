5bf619a905f9a41b101a9d36649110400e58820b
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2025-04-12 17:29:16 +0300

    IBaseBoltDbContext Fix

--- src/BiUM.Bolt/Database/IBaseBoltDbContext.cs
+++ src/BiUM.Bolt/Database/IBaseBoltDbContext.cs
@@ -8,10 +8,10 @@ public interface IBaseBoltDbContext : IDbContext
 {
     DbSet<BoltTransaction> BoltTransactions { get; }
 
-    Task<bool> AddOrUpdate<TEntity>(int order, string name, TEntity entity, CancellationToken cancellationToken) where TEntity : IBaseEntity;
-    Task<bool> AddOrUpdate<TEntity>(int order, string name, IList<TEntity> entities, CancellationToken cancellationToken) where TEntity : IBaseEntity;
-    Task<bool> Delete<TEntity>(int order, string name, TEntity entity, CancellationToken cancellationToken) where TEntity : IBaseEntity;
-    Task<bool> Delete<TEntity>(int order, string name, IList<TEntity> entities, CancellationToken cancellationToken) where TEntity : IBaseEntity;
+    Task<bool> AddOrUpdate<TEntity>(int order, string name, TEntity entity, CancellationToken cancellationToken) where TEntity : IEntity;
+    Task<bool> AddOrUpdate<TEntity>(int order, string name, IList<TEntity> entities, CancellationToken cancellationToken) where TEntity : IEntity;
+    Task<bool> Delete<TEntity>(int order, string name, TEntity entity, CancellationToken cancellationToken) where TEntity : IEntity;
+    Task<bool> Delete<TEntity>(int order, string name, IList<TEntity> entities, CancellationToken cancellationToken) where TEntity : IEntity;
 }
 
 public interface IBaseBoltDomainDbContext : IDbContext

b1836ae3664cd46b0ba0927f8284b3428608fa3f
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2025-04-12 15:04:31 +0300

    Exception Handler Fix

--- src/BiUM.Specialized/ConfigureApp.cs
+++ src/BiUM.Specialized/ConfigureApp.cs
@@ -20,7 +20,7 @@ public static partial class ConfigureApp
         }
         else
         {
-            app.UseExceptionHandler();
+            app.UseExceptionHandler(o => { });
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }

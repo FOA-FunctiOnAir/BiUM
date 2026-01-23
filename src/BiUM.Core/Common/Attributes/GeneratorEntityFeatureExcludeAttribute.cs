6ada53ebfd8817d3bec74f40902f7fc5bfa25752
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2026-01-24 03:32:47 +0400

    GeneratorEntityFeatureExcludeAttribute added

--- src/BiUM.Core/Common/Attributes/GeneratorEntityFeatureExcludeAttribute.cs
+++ src/BiUM.Core/Common/Attributes/GeneratorEntityFeatureExcludeAttribute.cs
@@ -5,16 +5,16 @@ using System.Collections.Generic;
 namespace BiUM.Core.Common.Attributes;
 
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
-public class GeneratorEntityFeatureAttribute : Attribute
+public class GeneratorEntityFeatureExcludeAttribute : Attribute
 {
     public IReadOnlyCollection<EntityFeatures> Features { get; }
 
-    public GeneratorEntityFeatureAttribute()
+    public GeneratorEntityFeatureExcludeAttribute()
     {
         Features = [EntityFeatures.All];
     }
 
-    public GeneratorEntityFeatureAttribute(params EntityFeatures[] features)
+    public GeneratorEntityFeatureExcludeAttribute(params EntityFeatures[] features)
     {
         Features = features?.Length > 0 ? features : [EntityFeatures.All];
     }

2a381bb20d500041e4ab95455e42b13b3c5484f4
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2026-02-21 07:02:48 +0300

    Fix

--- src/BiUM.Core/Common/Attributes/GeneratorEntityFeatureAttribute.cs
+++ src/BiUM.Core/Common/Attributes/GeneratorEntityFeatureAttribute.cs
@@ -7,15 +7,29 @@ namespace BiUM.Core.Common.Attributes;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 public class GeneratorEntityFeatureAttribute : Attribute
 {
+    public string? MainEntityName { get; }
     public IReadOnlyCollection<EntityFeatures> Features { get; }
 
     public GeneratorEntityFeatureAttribute()
     {
+        MainEntityName = string.Empty;
+        Features = [EntityFeatures.All];
+    }
+
+    public GeneratorEntityFeatureAttribute(string mainEntityName)
+    {
+        MainEntityName = mainEntityName;
         Features = [EntityFeatures.All];
     }
 
     public GeneratorEntityFeatureAttribute(params EntityFeatures[] features)
     {
+        Features = features?.Length > 0 ? features : [EntityFeatures.All];
+    }
+
+    public GeneratorEntityFeatureAttribute(string mainEntityName, params EntityFeatures[] features)
+    {
+        MainEntityName = mainEntityName;
         Features = features?.Length > 0 ? features : [EntityFeatures.All];
     }
 }

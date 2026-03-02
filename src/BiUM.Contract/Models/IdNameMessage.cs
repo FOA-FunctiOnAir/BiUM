cb6d371499b7b5cd79b63422f0e3e347b42bbaae
mehyaa <mehyaa@gmail.com>
2026-03-02 14:30:18 +0300

    Update IdNameMessage and BaseForValuesDto to enforce required properties for Id and Name

--- src/BiUM.Contract/Models/IdNameMessage.cs
+++ src/BiUM.Contract/Models/IdNameMessage.cs
@@ -1,10 +1,11 @@
 using MemoryPack;
+using System;
 
 namespace BiUM.Contract.Models;
 
 [MemoryPackable]
 public partial class IdNameMessage
 {
-    public required string Id { get; set; }
+    public required Guid Id { get; set; }
     public required string Name { get; set; }
 }
--- src/BiUM.Specialized/Common/Mapper/BaseMapper.cs
+++ src/BiUM.Specialized/Common/Mapper/BaseMapper.cs
@@ -5,7 +5,6 @@ namespace BiUM.Specialized.Common.Mapper;
 
 public class BaseForValuesDto<TType> : IMapFrom<TType>
 {
-    public Guid Id { get; set; }
-
-    public string? Name { get; set; }
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
 }
